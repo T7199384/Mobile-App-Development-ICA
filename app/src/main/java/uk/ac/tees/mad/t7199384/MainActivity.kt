7b6bc302ac67167bf10ddfd1531027eabaa2375b
T7199384 <55836934+T7199384@users.noreply.github.com>
2023-10-31 21:37:40 +0000

    Made Title larger Image is bigger

--- app/src/main/java/uk/ac/tees/mad/t7199384/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/mad/t7199384/MainActivity.kt
@@ -11,9 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import uk.ac.tees.mad.t7199384.ui.theme.ICATheme
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,18 +40,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(title: String, modifier: Modifier = Modifier) {
-    Row {
-        Image (
-            painter = painterResource(R.drawable.ic_launcher_foreground),
-            contentDescription = "Splash Screen Image",
-            modifier = modifier
-        )
-    }
-    Row {
-        Text(
-            text = "$title",
-            //modifier = modifier
-        )
+    Column{
+        Row {
+            Spacer(modifier = Modifier.height(30.dp))
+            Image (
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = "Splash Screen Image",
+                modifier = Modifier
+                    .size(500.dp)
+            )
+        }
+        Row {
+            Text(
+                text = "$title",
+                modifier = modifier,
+                style = MaterialTheme.typography.titleLarge
+            )
+        }
     }
 }
 
