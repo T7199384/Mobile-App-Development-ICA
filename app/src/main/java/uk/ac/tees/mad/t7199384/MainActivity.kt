d53dbb283718b8903671a633ef7de4de40061a41
T7199384 <55836934+T7199384@users.noreply.github.com>
2023-11-14 02:22:56 +0000

    Shared Preferences work Reverted layout to easier layout and changed FAB to elevated button

--- app/src/main/java/uk/ac/tees/mad/t7199384/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/mad/t7199384/MainActivity.kt
@@ -1,31 +1,32 @@
 package uk.ac.tees.mad.t7199384
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -33,8 +34,7 @@ import androidx.compose.ui.unit.dp
 import uk.ac.tees.mad.t7199384.ui.theme.ICATheme
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-    @OptIn(ExperimentalMaterial3Api::class)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         var sharedPref = this@MainActivity.getSharedPreferences(getString(R.string.world_file_key), Context.MODE_PRIVATE)
         var world = sharedPref.getString("world", "Empty").toString()
@@ -42,14 +42,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ICATheme {
-                Scaffold(
+                var worldText by remember{mutableStateOf(world)}
 
-                    floatingActionButtonPosition = FabPosition.End,
-                    floatingActionButton = { World_Button(world) },
-                    containerColor = MaterialTheme.colorScheme.background){
-
-                    Greeting2(world)
-                }
+                Surface( modifier = Modifier.fillMaxSize(),color = MaterialTheme.colorScheme.background) {
+                    Row(Modifier.fillMaxWidth()){
+                        Greeting2(worldText)
+                        Spacer(Modifier.weight(1f,true).fillMaxWidth().background(MaterialTheme.colorScheme.background))
+                        World_Button(world = worldText)
+                    }
 
 
             }
@@ -70,23 +70,25 @@ fun World_Button(world: String) {
             var sharedPref = context.getSharedPreferences(context.resources.getString(R.string.world_file_key), Context.MODE_PRIVATE)
             var edit = sharedPref.edit()
             edit.putString("world",currentWorld)
+            edit.apply()
             Toast.makeText(context, "Current world: $currentWorld", Toast.LENGTH_SHORT).show()}
         .setNegativeButton("Cancel"){dialog, which -> Toast.makeText(context, "Current world: $world", Toast.LENGTH_SHORT).show()}
         .setSingleChoiceItems(array,0,){ dialog, which -> currentWorld=array[which]}
 
-    Column(horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.Top) {
-        FloatingActionButton(
+        ElevatedButton(
             onClick = {
                 val dialog: AlertDialog = builder.create()
                 dialog.show()
             },
+            contentPadding = PaddingValues(0.dp),
             shape = CircleShape,
+            colors = ButtonDefaults.elevatedButtonColors(contentColor = Color.White),
             modifier = Modifier
-                .size(56.dp)
+                .size(50.dp)
         ) {
-            Icon(painterResource(R.drawable.world_icon), "Change World Server")
+            Icon(painterResource(R.drawable.world_icon), "Change World Server", tint= Color.DarkGray)
         }
-    }
+
 }
 
 
@@ -94,8 +96,7 @@ fun World_Button(world: String) {
 fun Greeting2(world: String) {
     var worldGreeting by remember{mutableStateOf(world)}
     Text(
-        text = "Welcome to the $worldGreeting marketboard!",
-        modifier = Modifier.fillMaxSize()
+        text = "Welcome to $worldGreeting's market!",
     )
 }
 
@@ -106,7 +107,14 @@ fun Greeting2(world: String) {
 fun GreetingPreview2() {
     var world = "Crystal"
     ICATheme {
-        Greeting2(world)
-        World_Button(world)
+        Surface( modifier = Modifier.fillMaxSize(),color = MaterialTheme.colorScheme.background) {
+            Row(Modifier.fillMaxWidth()){
+                Greeting2(world)
+                Spacer(Modifier.weight(1f,true).fillMaxWidth().background(MaterialTheme.colorScheme.background))
+                World_Button(world = "Crystal")
+                }
+            }
+        }
     }
 }
+
